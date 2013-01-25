e3ec29d6070639b521820c5dd0ecc1edff94c118
flubadoodoo <hamdanjaveed@gmail.com>
2013-01-25 18:01:34 -0500

    Alpha 0.1.9d
     - Moved project to Mac, no changes

--- Project Alpha/src/helper/Status.java
+++ Project Alpha/src/helper/Status.java
@@ -1,11 +1,11 @@
 package helper;
 
 public class Status {
-	
-	private static String	ProjectStatus	= "PixelPotato Project Alpha 0.1.9";
-	
-	public static String getProjectStatus() {
+
+	private static String	ProjectStatus	= "PixelPotato Project Alpha 0.1.9d";
+
+	public static String getProjectStatus () {
 		return ProjectStatus;
 	}
-	
+
 }
