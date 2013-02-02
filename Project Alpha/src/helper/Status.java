3745523e33584bda30b2404e0e226fe28dd9e9c0
flubadoodoo <hamdanjaveed@gmail.com>
2013-02-01 21:42:40 -0500

    Alpha 0.2.3_01  - Just a test for EGit

--- Project Alpha/src/helper/Status.java
+++ Project Alpha/src/helper/Status.java
@@ -2,7 +2,7 @@ package helper;
 
 public class Status {
 	
-	private static String	ProjectStatus	= "PixelPotato Project Alpha 0.2.3";
+	private static String	ProjectStatus	= "PixelPotato Project Alpha 0.2.3_01";
 	
 	public static String getProjectStatus() {
 		return ProjectStatus;
